5d89810982ef1dcb131b561e6041b06d2daa3ff1
AlexYi7852 <3092342855@qq.com>
2018-05-26 13:31:29 +0800

    自动轮播滚动到最后一张不能循环到第一张，1.0版本以下用this.slider.goToPage(),1.0以上用this.slider.next()

--- src/base/slider/index.vue
+++ src/base/slider/index.vue
@@ -12,14 +12,14 @@
 </template>
 
 <script>
-import BScroll from 'better-scroll'
-import { MyDom } from 'common/js/dom'
+import BScroll from "better-scroll";
+import { MyDom } from "common/js/dom";
 export default {
-  data () {
+  data() {
     return {
       dots: [],
       currentDotsIndex: 0 // 当前小点
-    }
+    };
   },
   props: {
     // 是否循环播放
@@ -35,43 +35,46 @@ export default {
     // 轮播延时
     interval: {
       type: Number,
-      default: 3000  
+      default: 3000
     }
   },
-  mounted () {
+  mounted() {
     setTimeout(() => {
-      this._setSliderWidth()
-      this._initDots()
-      this._initSlider()
-    }, 20)
+      this._setSliderWidth();
+      this._initDots();
+      this._initSlider();
+      if (this.autoPlay) {
+        this._initPlay();
+      }
+    }, 20);
   },
   methods: {
-    // 横向滚动需要设置slider(sliderGroup)宽度  
-    _setSliderWidth () {
-      // 首先获取slider(sliderGroup)的子元素数量  
-      this.children = this.$refs.sliderGroup.children
-      let width = 0 // 初始化父元素宽度
+    // 横向滚动需要设置slider(sliderGroup)宽度
+    _setSliderWidth() {
+      // 首先获取slider(sliderGroup)的子元素数量
+      this.children = this.$refs.sliderGroup.children;
+      let width = 0; // 初始化父元素宽度
       // 获取父元素的宽度, 每个子元素的宽度等于父元素的宽度
-      let sliderWidth = this.$refs.slider.clientWidth
+      let sliderWidth = this.$refs.slider.clientWidth;
       for (let i = 0; i < this.children.length; i++) {
-        let child = this.children[i]
+        let child = this.children[i];
         // 给每个子元素添加class类名, 并且设置宽度
-        MyDom.addClass(child, 'slider-item')
-        child.style.width = sliderWidth + 'px'
-        width += sliderWidth  // 累加计算出父元素的总宽度
+        MyDom.addClass(child, "slider-item");
+        child.style.width = sliderWidth + "px";
+        width += sliderWidth; // 累加计算出父元素的总宽度
       }
       // 如果可以无缝循环轮播，父元素的宽度要加两个子元素的宽度
       if (this.loop) {
-        width += 2 * sliderWidth  
+        width += 2 * sliderWidth;
       }
-      this.$refs.sliderGroup.style.width = width + 'px'
+      this.$refs.sliderGroup.style.width = width + "px";
     },
     // 初始化slider
-    _initSlider () {
+    _initSlider() {
       this.slider = new BScroll(this.$refs.slider, {
-        click: true,        // 是否允许点击
-        scrollX: true,      // 是否允许横向滚动
-        scrollY: false,     // 是否允许纵向滚动
+        click: true, // 是否允许点击
+        scrollX: true, // 是否允许横向滚动
+        scrollY: false, // 是否允许纵向滚动
         // 现象:
         //     可以轮播,但不能循环(不能从第一张往前翻,不能从最后一张往后翻)
         //     按教程里给slider-group增加了2个slider的宽度,但他们出现在轮播图片的末尾(2个空白)
@@ -81,28 +84,47 @@ export default {
         snap: {
           loop: this.loop, // 是否可以无缝循环轮播
           snapThreshold: 0.3, // 用手指滑动时页面可切换的阈值，大于这个阈值可以滑动的下一页
-          snapSpeed: 400     // 轮播图切换的动画时间
-        },         // 该属性是给 slider 组件使用的，普通的列表滚动不需要配置
-        momentum: true     // 当快速滑动时是否开启滑动惯性
-      })
+          snapSpeed: 400 // 轮播图切换的动画时间
+        }, // 该属性是给 slider 组件使用的，普通的列表滚动不需要配置
+        momentum: false // 当快速滑动时是否开启滑动惯性
+      });
       // 滚动完毕的时候获取它的pageIndex
-      this.slider.on('scrollEnd', () => {
-        // 获取当前小点  
-        let dotsIndex = this.slider.getCurrentPage().pageX
-        // 循环模式下 -1
-        this.currentDotsIndex = dotsIndex
-      })
+      this.slider.on("scrollEnd", () => {
+        // 获取当前小点
+        let dotsIndex = this.slider.getCurrentPage().pageX;
+        // 老版本有,新版去掉:
+        // if (this.loop) {
+        //   pageIndex -= 1
+        // }
+        this.currentDotsIndex = dotsIndex;
+        if (this.autoPlay) {
+          clearTimeout(this.timer); //用手拖动的时候
+          this._initPlay();
+        }
+      });
     },
     // 获取dots长度
-    _initDots () {
-      this.dots = new Array(this.children.length)  
+    // 老版本后dots会多两个
+    _initDots() {
+      this.dots = new Array(this.children.length);
+    },
+    _initPlay() {
+      let pageIndex = this.currentDotsIndex;
+      if (this.loop) {
+        pageIndex += 1
+      }
+      this.timer = setTimeout(() => {
+        // 1.0版本以上用next()
+        // 1.0以下用goToPage， goToPage是better-scroll内置方法
+        this.slider.next()
+      }, this.interval)
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-@import 'common/scss/variable';
+@import "common/scss/variable";
 
 .slider {
   min-height: 1px;
