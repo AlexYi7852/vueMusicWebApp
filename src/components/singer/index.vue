8e178986d09f2ad424e24eb281ca9330a39fc77c
AlexYi7852 <3092342855@qq.com>
2018-05-28 03:35:31 +0800

    把歌手列表处理成了我们需要的有序列表

--- src/components/singer/index.vue
+++ src/components/singer/index.vue
@@ -1,12 +1,17 @@
 
 <template>
-  <div class="singer">歌手</div>  
+  <div class="singer">
+
+  </div>  
 </template>
 
 <script>
 import Scroll from 'base/scroll'
 import { MySinger } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10 
 export default {
   data () {
     return {
@@ -21,9 +26,53 @@ export default {
       MySinger.getSingerList().then((res) => {
         if (res.code === ERR_OK) {
           this.singerList = res.data.list
-          console.log(this.singerList)
+          console.log(this._normalizeSinger(this.singerList))
         }
       })
+    },
+    // 组合我们所需要的数据
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        // 组合热门数据列表
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 为了得到有序列表， 我们需要处理map
+      let hot = []
+      let rest = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          rest.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      rest.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(rest)
     }
   }
 }
