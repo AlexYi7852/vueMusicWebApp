dccfdd4a7b0616a1b89c60947707e493b9db4aab
AlexYi7852 <3092342855@qq.com>
2018-06-04 23:55:43 +0800

    拖动进度条按钮功能实现

--- src/base/progressBar/index.vue
+++ src/base/progressBar/index.vue
@@ -1,11 +1,14 @@
 
 <template>
-  <div class="progress-bar" ref="progressBar">
+  <div class="progress-bar" ref="barRef">
     <div class="bar-inner">
         <!-- 已播放的进度 -->
-      <div class="progress" ref="progress"></div>
+      <div class="progress" ref="progressRef"></div>
       <!-- 按钮 -->
-      <div class="progress-btn-wrapper" ref="progressBtn">
+      <div class="progress-btn-wrapper" ref="btnRef"
+                         @touchstart.prevent="progressTouchStart"
+                           @touchmove="progressTouchMove"
+                             @touchend="progressTouchEnd">
         <div class="progress-btn"></div>
       </div>  
     </div>
@@ -16,27 +19,58 @@
 // 引入transform 前缀
 import { prefixStyle } from 'common/js/dom'
 // 按钮宽度
-const progressBtnWidth = 16
+const BTN_WIDTH = 16
 const transform = prefixStyle('transform')
 export default {
   props: {
-    // 百分比
+    // 当前播放进度 [0, 1]
     percent: {
       type: Number,
       default: 0  
     }  
   },
+  created () {
+    // 共享数据对象
+    this.touch = {}
+  },
+  methods: {
+    // 拖动小球改变播放进度，小球滑动开始位置
+    progressTouchStart (e) {
+      this.touch.init = true
+      // 开始滑动的位置
+      this.touch.startX = e.touches[0].pageX
+      // 当前偏移
+      this.touch.left = this.$refs.progressRef.clientWidth
+    },
+    // 拖动小球改变播放进度，小球滑动过程中
+    progressTouchMove (e) {
+      if (!this.touch.init) return
+
+      // 滑动的差值
+      let deltaX = e.touches[0].pageX - this.touch.startX
+      // 进度条的差值，大于0，小于总长度
+      let offsetWidth = Math.min(this.$refs.barRef.clientWidth - BTN_WIDTH, Math.max(0, this.touch.left + deltaX))
+
+      this._move(offsetWidth)
+    },
+    // 拖动小球改变播放进度，小球滑动结束
+    progressTouchEnd () {
+      this.touch.init = false
+    },
+    // 进度条前进 + 小球前进
+    _move(offsetWidth) {
+      this.$refs.progressRef.style.width = `${offsetWidth}px`
+      this.$refs.btnRef.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+    }
+  },
   watch: {
-    percent (newPercent) {
-      if(newPercent >= 0) {
-        // 首先获取progressBar的宽度  
-        let barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
-        // 按钮偏移的宽度
-        let offsetWidth = newPercent * barWidth
-        // 播放的进度的宽度
-        this.$refs.progress.style.width = `${offsetWidth}`
-        // 按钮偏移量
-        this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+    // 播放进度
+    percent(newVal) {
+      // 拖动的时候不要 watch
+      if (newVal >= 0 && !this.touch.init) {
+        let barWidth = this.$refs.barRef.clientWidth - BTN_WIDTH
+        let offsetWidth = newVal * barWidth
+        this._move(offsetWidth)
       }
     }
   }
