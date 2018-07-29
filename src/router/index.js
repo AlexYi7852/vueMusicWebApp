a8a752c67aadd3bcbb80551febbd648960f6e96c
易烨 <ex_yiye@xiaoniu66.com>
2018-07-29 18:32:55 +0800

    使用vue的异步组件技术，实现路由按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,53 @@
-
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend' // 推荐页
-import Ranking from 'components/ranking' // 排行榜
-import Search from 'components/search' // 搜索页面
-import Singer from 'components/singer' // 歌手页面
-import UserCenter from 'components/userCenter' // 个人中心
-import SingerDetail from 'components/singerDetail' // 歌手详情页面
 
 Vue.use(Router)
 
 export default new Router({
-  scrollBehavior (to, from, savedPosition) {
-    console.log(to, 'to', from, 'from', savedPosition)
+  scrollBehavior(to, from, savedPosition) {
+    // console.log(to, 'to', from, 'from', savedPosition)
     if (savedPosition) {
       return savedPosition
     } else {
-      return { x: 0, y: 0 }
+      return {
+        x: 0,
+        y: 0
+      }
     }
   },
   routes: [
-    { path: '/', redirect: '/recommend' },
-    { path: '/recommend', component: Recommend, meta: { keepAlive: true } },
-    { path: '/ranking', component: Ranking },
-    { path: '/search', component: Search },
     {
-      path: '/singer',
-      component: Singer,
-      meta: { keepAlive: false },
-      children: [
-        {
-          path: ':id', // 表示 id 为变量
-          component: SingerDetail
-        }
-      ]
+      path: '/',
+      redirect: '/recommend'
     },
-    { path: '/user', component: UserCenter }
+    {
+      path: '/recommend', // 推荐页
+      meta: { title: '推荐', keepAlive: true },
+      component: resolve => require(['components/recommend'], resolve)
+    },
+    {
+      path: '/ranking', // 排行榜
+      meta: { title: '排行榜' },
+      component: resolve => require(['components/ranking'], resolve)
+    },
+    {
+      path: '/search', // 搜索页面
+      meta: { title: '搜索' },
+      component: resolve => require(['components/search'], resolve)
+    },
+    {
+      path: '/singer', // 歌手页面
+      meta: { title: '歌手', keepAlive: false},
+      component: resolve => require(['components/singer'], resolve),
+      children: [{
+        path: ':id', // 歌手详情页面  id 为变量
+        component: resolve => require(['components/singerDetail'], resolve)
+      }]
+    }, 
+    {
+      path: '/user', // 个人中心
+      meta: { title: '个人中心' },
+      component: resolve => require(['components/userCenter'], resolve)
+    }
   ]
 })
